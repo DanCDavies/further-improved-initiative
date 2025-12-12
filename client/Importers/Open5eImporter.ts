6c849497e079f55937cd1d6580de411ed786a7d5
Evan Bailey <cynicaloptimist@gmail.com>
2025-12-11 21:54:10 -0800

    Add importer for Open5e v2 creature statblocks

--- client/Importers/Open5eImporter.ts
+++ client/Importers/Open5eImporter.ts
@@ -175,3 +175,138 @@ function getType(data: any): string {
   }
   return typeString;
 }
+
+export function ImportOpen5eV2StatBlock(
+  open5eStatBlock: Record<string, any>
+): StatBlock {
+  // https://api.open5e.com/v2/creatures/?document__key=srd-2024&limit=20
+  const sb = open5eStatBlock;
+  return {
+    ...StatBlock.Default(),
+    Name: sb.name,
+    Source: sb.document.display_name,
+    Type: getTypeV2(sb),
+    HP: {
+      Value: sb.hit_points,
+      Notes: parenthesizeOrEmpty(sb.hit_dice)
+    },
+    AC: {
+      Value: sb.armor_class,
+      Notes: parenthesizeOrEmpty(sb.armor_detail)
+    },
+    InitiativeModifier:
+      (sb.initiative_bonus ?? 0) - (sb.modifiers?.dexterity ?? 0),
+    Speed: getSpeedV2(sb),
+    Abilities: {
+      Str: sb.ability_scores.strength,
+      Dex: sb.ability_scores.dexterity,
+      Con: sb.ability_scores.constitution,
+      Int: sb.ability_scores.intelligence,
+      Wis: sb.ability_scores.wisdom,
+      Cha: sb.ability_scores.charisma
+    },
+    DamageVulnerabilities:
+      sb.resistances_and_immunities.damage_vulnerabilities.map(v => v.name),
+    DamageResistances: sb.resistances_and_immunities.damage_resistances.map(
+      v => v.name
+    ),
+    DamageImmunities: sb.resistances_and_immunities.damage_immunities.map(
+      v => v.name
+    ),
+    ConditionImmunities: sb.resistances_and_immunities.condition_immunities.map(
+      v => v.name
+    ),
+    Saves: Object.keys(sb.saving_throws ?? {}).map(saveName => {
+      return {
+        Name: _.startCase(saveName),
+        Modifier: sb.saving_throws[saveName]
+      };
+    }),
+    Skills: Object.keys(sb.skill_bonuses ?? {}).map(skillName => {
+      return {
+        Name: _.startCase(skillName),
+        Modifier: sb.skill_bonuses[skillName]
+      };
+    }),
+    Senses: getSensesV2(sb),
+    Languages: commaSeparatedStrings(sb.languages.as_string),
+    Challenge: sb.challenge_rating,
+    Traits: nameAndDescArraysV2(sb.traits),
+    Actions: nameAndDescArraysV2(
+      sb.actions.filter(a => a.action_type === "ACTION")
+    ),
+    BonusActions: nameAndDescArraysV2(
+      sb.actions.filter(a => a.action_type === "BONUS_ACTION")
+    ),
+    LegendaryActions: nameAndDescArraysV2(
+      sb.actions.filter(a => a.action_type === "LEGENDARY_ACTION")
+    ),
+    MythicActions: nameAndDescArraysV2(
+      sb.actions.filter(a => a.action_type === "MYTHIC_ACTION")
+    ),
+    Reactions: nameAndDescArraysV2(
+      sb.actions.filter(a => a.action_type === "REACTION")
+    )
+  };
+}
+
+function getTypeV2(data: any): string {
+  const sizeString = data.size.name;
+  const typeString = data.type.name;
+  const alignmentString = data.alignment;
+  let fullType = "";
+  if (sizeString) {
+    fullType += sizeString + " ";
+  }
+  fullType += typeString;
+  if (alignmentString) {
+    fullType += ", " + alignmentString;
+  }
+  return fullType;
+}
+
+function getSpeedV2(data: any): string[] {
+  const speeds: string[] = [];
+  const { unit, fly, hover, ...rest } = data.speed;
+  for (const speedType in rest) {
+    speeds.push(`${speedType} ${data.speed[speedType]} ${unit}`);
+  }
+  if (fly) {
+    speeds.push(`fly ${data.speed.fly} ${unit}` + (hover ? " (hover)" : ""));
+  }
+  return speeds;
+}
+
+function getSensesV2(data: any): string[] {
+  const senses: string[] = [];
+  if (data.darkvision_range) {
+    senses.push(`darkvision ${data.darkvision_range} ft.`);
+  }
+  if (data.tremorsense_range) {
+    senses.push(`tremorsense ${data.tremorsense_range} ft.`);
+  }
+  if (data.truesight_range) {
+    senses.push(`truesight ${data.truesight_range} ft.`);
+  }
+  if (data.blindsight_range) {
+    senses.push(`blindsight ${data.blindsight_range} ft.`);
+  }
+  return senses;
+}
+
+function nameAndDescArraysV2(entries: any): NameAndContent[] {
+  if (!entries?.map) {
+    return [];
+  }
+  return entries.map(getNameAndContentV2);
+}
+
+function getNameAndContentV2(data: {
+  name: string;
+  desc: string;
+}): NameAndContent {
+  return {
+    Name: data.name,
+    Content: data.desc
+  };
+}
